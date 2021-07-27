927f35f2d6e1fd09c5e1d1c7a8fc9e4f8743cb04
Artem Derenok <derenokartem95@gmail.com>
2021-07-28 00:45:44 +0300

    create webpack config

---
+++ react-components/src/components/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App(): JSX.Element {
+  return (
+    <div className="heading">
+      <img src="avatar.jpg" alt="avatar" />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-components/webpack.config.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlMinimizerPlugin = require("html-minimizer-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    open: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: "./src/index.html"}),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/assets'),
+          to: path.resolve(__dirname, 'dist')
+        },
+        {
+        
+          from: path.resolve(__dirname, "./src/assets"),
+          to: path.resolve(__dirname, "./dist"),
+          noErrorOnMissing: true,
+          }
+      ],
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+    minimize: true,
+    minimizer: [
+      new HtmlMinimizerPlugin({
+        test: /\.foo\.html/i,
+      }),
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader' ]
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(mp3)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.m?tsx/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env", "@babel/react", "@babel/typescript"]
+          }
+        }
+      }
+    ]
+  }
+}
