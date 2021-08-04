67b1e691ee2cc6103fdac409dfc326b9024886e5
Artem Derenok <derenokartem95@gmail.com>
2021-08-04 23:10:41 +0300

    feat: create card component and search

--- react-components/src/components/App.tsx
+++ react-components/src/components/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import './app.css';
+import { ICardProps } from '../interface/interfaces';
+import './App.css';
+import Card from './Card';
+import SearchBar from './SearchBar';
+
+const cardsArr: Array<ICardProps> = [
+  {
+    heading: 'Heading1',
+    text: 'Lorem ipsum dolor sit amet, Integer consectetur adipiscing elit.',
+    img: 'cat.jpg',
+  },
+  {
+    heading: 'Heading2',
+    text: 'Lorem ipsum dolor sit amet, Integer consectetur adipiscing elit. Integer varius accumsumsan massa id aliquam.',
+    img: 'smile.jpg',
+  },
+];
 
 function App(): JSX.Element {
   return (
-    <div className="heading">
-      <img src="avatar.jpg" alt="avatar" />
+    <div className="container">
+      <SearchBar />
+      <div className="card-container">
+        <Card heading={cardsArr[0].heading} text={cardsArr[0].text} img={cardsArr[0].img} />
+        <Card heading={cardsArr[1].heading} text={cardsArr[1].text} img={cardsArr[1].img} />
+      </div>
     </div>
   );
 }
