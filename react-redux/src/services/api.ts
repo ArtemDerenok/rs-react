5ea43f334adb9407ea74d5e90ba6a9094eee6102
ArtemDerenok <78560937+ArtemDerenok@users.noreply.github.com>
2021-08-24 19:30:22 +0300

    Delete react-redux directory

--- react-redux/src/services/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const axiosInstance = axios.create({
-  baseURL: 'https://newsapi.org/',
-  timeout: 10000,
-});
-
-export default axiosInstance;
--- react-redux/src/store/action-creator/sort-type.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch } from 'react';
-import { ISortAction, VALUE_SORT } from '../../intefaces/interfaces';
-
-export const choiseSortType = (value: string) => {
-  return (dispatch: Dispatch<ISortAction>): void => {
-    dispatch({ type: VALUE_SORT, payload: value });
-  };
-};
