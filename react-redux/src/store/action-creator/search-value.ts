a5361eec0db13f711bb2a7609ae774994a9f36c7
ArtemDerenok <78560937+ArtemDerenok@users.noreply.github.com>
2021-08-28 20:16:30 +0300

    Delete react-redux directory

--- react-redux/src/store/action-creator/search-value.ts
+++
@@ -1,8 +0,0 @@
-import { Dispatch } from 'react';
-import { ISearchAction, VALUE_SEARCH } from '../../intefaces/interfaces';
-
-export const choiseSearchValue = (value: string) => {
-  return (dispatch: Dispatch<ISearchAction>): void => {
-    dispatch({ type: VALUE_SEARCH, payload: value });
-  };
-};
